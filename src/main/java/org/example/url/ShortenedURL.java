d84abcd077be747c0ce8d7d37edd008d62cdfc7d
KhanFirstSearch <yousufskhan2005@gmail.com>
2024-06-25 07:34:20 -0500

    fixed a bug

--- src/main/java/org/example/url/ShortenedURL.java
+++ src/main/java/org/example/url/ShortenedURL.java
@@ -12,13 +12,14 @@ import java.time.LocalDateTime;
 public class ShortenedURL {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(nullable = false, unique = true) //Prevents duplicated URLs
     private String shortenedURL;
 
-    @Column(nullable = false) //URLs should NOT be null
+    @Column(nullable = false) //URLs should NOT be null.
     private String originalURL;
 
-    private Long id;
     private LocalDateTime createdAt;
     private LocalDateTime expiryDate;
 }
